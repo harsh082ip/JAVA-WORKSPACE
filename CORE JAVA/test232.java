ee94c6c6899d3801737c7157895e9d3cc0577092
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-03-25 16:40:11 +0530

    chnages commited

--- CORE JAVA/test232.java
+++ CORE JAVA/test232.java
@@ -1,11 +1,37 @@
+
 public class test232 {
     public static void main(String[] args) {
-        String[] arr = {"c","f","j"};
-        String t = "a";
-        char ch = (String)arr[0];
-        if(arr[0] > arr[1])
-        {
+        int [] arr= {2,3,4,5,6,7,8,9,15,17,19,30,36,37,38,39,40};
+        int target=15;
+        int ans= searchrange(arr, target);
+        System.out.print(ans);
 
-        }
     }
+    static int searchrange(int [] arr, int target){
+        int start=0;
+        int end=1;
+        while(target>arr[end]){
+            int temp= end+1;
+             end = end+(end-start+1)*2;
+             start= temp;
+        }
+        return binarysearch(arr, target,start, end);
+    }
+    static int binarysearch(int[] arr, int target, int start, int end){
+        
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if (target>arr[mid]){
+                start= mid+1;
+            }
+            else if(target<arr[mid]){
+                end= mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    
 }
