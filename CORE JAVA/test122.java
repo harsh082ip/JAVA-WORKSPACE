57129150be9f684e39e181de3beb0219dd862241
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-03-31 05:49:30 +0530

    Some Changes Made

--- CORE JAVA/test122.java
+++ CORE JAVA/test122.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class test122 {
-    public static void main(String[] args) {
-        System.out.println(1%3);
+    public static void main(String[] args) { 
+    int arr[]= {3, 0, 1};
+    
+    System.out.println(sort(arr));
     }
+    static int sort(int[] arr){
+    int i=0;
+    while(i < arr.length){
+        int correct = arr[i];
+        if(arr[i] < arr.length && arr[i]!=arr[correct]){
+            int temp= arr[i];
+            arr[i]= arr[correct];
+            arr[correct]=temp;
+        }
+        else{
+            i++;
+        }
+    }
+    for(int index=0; index < arr.length; index++){
+        if(arr[index]!= index){
+            return index ;
+        }
+        
+    } 
+    return arr.length;
+     
+}
 }
