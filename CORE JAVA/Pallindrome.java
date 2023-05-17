2672be1d3245c317cae51b8b264c4ee999d289fb
ank809 <ankita51523@gmail.com>
2023-05-17 22:54:36 +0530

    Fixed Palindrome.java

--- CORE JAVA/Pallindrome.java
+++ CORE JAVA/Pallindrome.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Pallindrome {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a Number To Reverse: ");
+        System.out.println("Enter a Number you want to check is palindome or not  ");
         int x=sc.nextInt();
         int rev=0;
         int n=x;
@@ -11,6 +11,12 @@ public class Pallindrome {
             int d=x%10;
             rev=(rev*10)+d;
             x/=10;
+        }
+        if(n==rev){
+            System.out.println("Number is palindrome");
+        }
+        else{
+            System.out.println("Number is not palindome");
         }
        
     }
